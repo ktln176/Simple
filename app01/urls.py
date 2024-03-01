f8c14dd9d5e11ef17555e95e8f3df19c55d07627
ktln176 <ktln176@163.com>
2024-03-01 16:48:04 +0800

    update 20240301

---
+++ app01/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("demo/", views.demo, name="demo"),
+    path("home/", views.home, name="home"),
+    path("", views.start, name="start")
+]
